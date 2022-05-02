9da70aa2da174d06b710f45b84c3b52cf4e2abe5
d4rktell <olegbelinski1399@gmail.com>
2022-05-03 01:55:43 +0300

    install some

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import cn from 'classnames';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <div></div>
+            </Route>
+            <Route path="/users">
+              <div className={cn("text-9xl flex text-blue")}>
+                dfdfdff
+              </div>
+            </Route>
+            <Route path="/">
+              <div></div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ front/tailwind.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    content: ["./src/**/*.{jsx,js,ts,tsx}"],
+    theme: {
+        screens: {
+            "xs": "0px",
+            "sm": "577px",
+            "md": "769px",
+            "gl": "1024px",
+            "xl": "1201px"
+        },
+        extend: {
+            colors: {
+                'blue': '#243c5a',
+            },
+        },
+        plugins: [],
+    }
+}
